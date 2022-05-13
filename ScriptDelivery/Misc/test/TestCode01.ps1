07b4262eea1f0c50a80e5714a4e23ae1c91b1772
tgiqfe <tqji31001@gmail.com>
2022-05-13 21:13:46 +0900

    Add testcode.

---
+++ ScriptDelivery/Misc/test/TestCode01.ps1
@@ -0,0 +1,25 @@
+﻿# テスト用コード
+
+################################################################
+# Prepare
+################################################################
+
+$url = "http://localhost:5000"
+
+
+
+################################################################
+# Main
+################################################################
+
+# / へのGetアクセス
+Invoke-RestMethod -Method Get -Uri "${url}/"
+
+# / へのPostアクセス可否
+Invoke-RestMethod -Method Post -Uri "${url}/"
+
+# /map へアクセスしてMapping情報を取得
+$res = Invoke-RestMethod -Method Post -Uri "${url}/map"
+Write-Host $res.Heaers
+Write-Host "MappingList(JSON):`r`n" + $res.Body
+
